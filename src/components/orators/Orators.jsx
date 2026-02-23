535544635fe5476d0979ccecb1e1707221d41f1a
Helix-Synergy <info.helixconferences@gmail.com>
2026-02-23 16:04:19 +0530

    Updated

--- src/components/orators/Orators.jsx
+++ src/components/orators/Orators.jsx
@@ -24,7 +24,7 @@ const Orators = () => {
         ))}
       </div>
 
-      <Link to= "/nurselead-orators">
+      <Link to= "/nursesummit-orators">
         <button className="bg-one text-white px-10 md:px-20 py-2 rounded-full mt-6 text-sm md:text-base">
           View All
         </button>
