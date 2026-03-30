1a2d0fd34cbb919e99123012ec33d9206038b85f
Helix-Synergy <info.helixconferences@gmail.com>
2026-03-30 17:40:38 +0530

    Updated

--- src/components/ui/QuickLinks.jsx
+++ src/components/ui/QuickLinks.jsx
@@ -26,7 +26,7 @@ const links = [
     id: 'brochure-download',
     icon: Download,
     label: 'Brochure download',
-    link: '/FOODMICROBIOME_brochure.pdf'
+    link: '/NurseSummit.pdf'
   },
   {
     id: 'event-schedule',
